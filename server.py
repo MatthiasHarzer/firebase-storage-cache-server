66f81ad73190562620a750526e7fb67dfff4311f
Matthias Harzer <matthias.harzer03@gmail.com>
2023-05-28 11:40:39 +0200

    support any file type

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
+import base64
+import gzip
+import json
 import os
+import time
 import urllib.request
+from typing import IO
 
 from fastapi import FastAPI, Request
 from fastapi.responses import Response
 
 CACHE_DIR = "./cache"
 FIREBASE_BASE_URL = "firebasestorage.googleapis.com"
-IMAGE_RESPONSES = {200: {"content": {"image/png": {}}}}
 
 app = FastAPI()
 
@@ -14,29 +18,41 @@ if not os.path.exists(CACHE_DIR):
     os.makedirs(CACHE_DIR)
 
 
-@app.get("/", responses=IMAGE_RESPONSES, response_class=Response)
-def root(url: str, request: Request):
-    remaining_query_params = str(request.url).split("?")[2] if len(str(request.url).split("?")) > 2 else ""
+def save(file: str, download_url: str) -> None:
+    if os.path.exists(file):
+        return
 
+    with urllib.request.urlopen(download_url) as response:
+        content_type = response.getheader("Content-Type")
+
+        file_data = json.dumps({
+            "content_type": content_type,
+            "content": base64.encodebytes(response.read()).decode("utf-8")
+        }).encode("utf-8")
+
+        with open(file, "wb") as f:
+            f.write(file_data)
+
+
+@app.get("/")
+def root(url: str, request: Request):
     no_query_url = url.split("?")[0]
     path = no_query_url.split(FIREBASE_BASE_URL)[1]
-    storage_base_url = no_query_url.split("/o/")[0] + "/o/"
     path = path.split("/o/")[1]
 
-    path_encoded = path.replace("/", "%2F")
-
-    download_url = storage_base_url + path_encoded + "?" + remaining_query_params
+    download_url = "=".join(str(request.url).split("=")[1:])
 
     directory = os.path.join(CACHE_DIR, os.path.dirname(path))
+
     file = os.path.join(CACHE_DIR, path + ".cache")
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     if not os.path.exists(file):
-        with urllib.request.urlopen(download_url) as response:
-            with open(file, "wb") as f:
-                f.write(response.read())
+        save(file, download_url)
 
     with open(file, "rb") as f:
-        return Response(content=f.read(), media_type="image/png")
+        content = json.loads(f.read().decode("utf-8"))
+        image_content = base64.b64decode(content["content"])
+        return Response(content=image_content, media_type=content["content_type"])
